1023cbab2ffb37dd4c8a7592ea493a44eb93141d
Timothy Moon <tim@Timothys-MacBook-Pro.local>
2021-12-20 14:56:18 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Tim's Blog",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "297399968",
+      },
+    },
+  ],
+};
