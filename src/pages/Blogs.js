ae3faa5e96b348c0ad4742fe92717b2526c68b49
Timothy Moon <timhgmoon@gmail.com>
2021-12-22 01:35:17 -0800

    .

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -8,9 +8,9 @@ import Pagination from 'react-bootstrap/Pagination'
 
 const Blogs = ({articles}) => {
   
-  const populateArticles = articles.map((article) => {
-    return <Article key={article.page} page={'/' + article.page} title={article.title} content={article.content.slice(0, 300) + ' ...'}></Article>
-  })
+  const populateArticles = articles.map((article) => 
+    <Article key={article.page} page={'/' + article.page} title={article.title} content={article.content.slice(0, 300) + ' ...'}></Article>
+  )
 
   return (
     <div>
