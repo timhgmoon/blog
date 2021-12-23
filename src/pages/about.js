fabe17b1a2b0fdbf5b28e36baee897761eacac1b
Timothy Moon <timhgmoon@gmail.com>
2021-12-22 22:16:17 -0800

    fixed some errors with linking

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ const About = () => {
       <MyNav></MyNav>
       <div className={container}>
         <h1 className={title}>About Me</h1>
-        <p>Just a personal blog that I can refer back to for recent leetcode problems and projects. I've also included a discord bot that will send leetcode questions at a certain time interval. <a className={discord} href="https://discord.gg/AFkUgVbN" target="_blank">Feel free to join here.</a> </p>
+        <p>Just a personal blog that I can refer back to for recent leetcode problems and projects. I've also included a discord bot that will send leetcode questions at a certain time interval. <a className={discord} rel="noreferrer" href="https://discord.gg/AFkUgVbN" target="_blank">Feel free to join here.</a> </p>
         <ContactForm></ContactForm>
       </div>
       
